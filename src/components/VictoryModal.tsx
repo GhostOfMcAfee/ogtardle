6031decc78175d7b18a5a4a6acdddde8a632ef83
GhostOfMcafee <your.github@email.com>
2025-05-01 08:01:35 -0500

    Update Twitter share emojis to custom set

--- src/components/VictoryModal.tsx
+++ src/components/VictoryModal.tsx
@@ -25,13 +25,13 @@ const generateTwitterText = (guesses: string[][], letterStates: Record<string, L
           const state = letterStates[rowIndex][letter.toLowerCase()];
           switch (state) {
             case LetterState.Blue:
-              return '🟦';
+              return '🤑';
             case LetterState.Correct:
-              return '🟩';
+              return '🌈';
             case LetterState.Present:
-              return '🟨';
+              return '🫵';
             case LetterState.Absent:
-              return '🟥';
+              return '🤡';
             default:
               return '';
           }
